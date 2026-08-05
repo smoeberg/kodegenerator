58861a0d6ad00896ecdbc1b7cca1317db67b8c73
WM-group <moeberg@wm-group.dk>
2026-08-05 09:10:34 +0200

    phase1: expose runtime contracts

--- runtime/__init__.py
+++ runtime/__init__.py
@@ -1,1 +1,13 @@
 """DOR Foundation v0.1 runtime."""
+
+from .context import ContextError, OrganizationContext, establish_context
+from .core import DORRuntime, NotFoundError, RuntimeNotReadyError
+
+__all__ = [
+    "ContextError",
+    "OrganizationContext",
+    "establish_context",
+    "DORRuntime",
+    "NotFoundError",
+    "RuntimeNotReadyError",
+]
