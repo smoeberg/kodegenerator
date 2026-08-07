bdd137495a39893c6fbbd5ca8348733314e47f88
WM-group <moeberg@wm-group.dk>
2026-08-07 14:18:01 +0200

    feat(p3-16): expose project renderer

--- generation/__init__.py
+++ generation/__init__.py
@@ -1,11 +1,15 @@
-"""Deterministic project architecture and scaffold generation."""
+"""Deterministic project architecture, scaffolding, and rendering."""
 
+from generation.project_renderer import ProjectRenderer, RenderedFile, RenderedProject
 from generation.project_spec import ArchitectureKind, ProjectDefinition
 from generation.scaffold_engine import ScaffoldEngine, ScaffoldPlan
 
 __all__ = [
     "ArchitectureKind",
     "ProjectDefinition",
+    "ProjectRenderer",
+    "RenderedFile",
+    "RenderedProject",
     "ScaffoldEngine",
     "ScaffoldPlan",
 ]
