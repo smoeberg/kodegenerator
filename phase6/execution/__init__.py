0956275f66e5ed8423ded2938d35f402fb9be906
WM-group <moeberg@wm-group.dk>
2026-08-10 08:24:54 +0200

    feat: export process sandbox adapter

--- phase6/execution/__init__.py
+++ phase6/execution/__init__.py
@@ -1,5 +1,6 @@
 """Phase 6 execution isolation contracts."""
 
+from phase6.execution.process import BubblewrapProcessAdapter, ProcessSandboxUnavailable
 from phase6.execution.sandbox import (
     ExecutionLimits,
     ExecutionResult,
@@ -11,10 +12,12 @@ from phase6.execution.sandbox import (
 )
 
 __all__ = [
+    "BubblewrapProcessAdapter",
     "ExecutionLimits",
     "ExecutionResult",
     "ExecutionSecurityContext",
     "ExecutionSpec",
+    "ProcessSandboxUnavailable",
     "Sandbox",
     "SandboxAdapter",
     "SandboxRegistry",
